f4cb2d3b17a170a2cbd83bbc5b44609891d7fb52
umangdivyanshu <udivyanshu8@gmail.com>
2021-08-01 14:38:53 +0530

    Segregating test files

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,5 +1,9 @@
 import './login_test.dart' as loginTest;
+import './addwatches_test.dart' as addWatches;
+import './cart_test.dart' as addToCart;
 
 void main() {
   loginTest.main();
+  addWatches.main();
+  //addToCart.main();
 }
--- integration_test/cart_test.dart
+++ integration_test/cart_test.dart
@@ -1,1 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:pocwatchapp/main.dart';
+import 'package:pocwatchapp/screens/cart_page.dart';
+import 'package:pocwatchapp/screens/home_screen.dart';
+import 'package:pocwatchapp/screens/login_screen.dart';
 
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets("adding and removing items in the cart",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await tester.pumpAndSettle(Duration(seconds: 2));
+    print('App launched');
+    expect(
+        find.text(
+            'This is a watch shopping app in which you can browser different watches, add to cart, add new products'),
+        findsOneWidget);
+
+    //identifying using widget type
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pumpAndSettle();
+
+    //validating user has moved to Login Page
+    expect(find.byType(LoginScreen), findsOneWidget);
+
+    //entering email
+    var emailInput = find.byKey(ValueKey('loginTxtKey'));
+    await tester.enterText(emailInput, 'user');
+
+    //entering password
+    var passwordInput = find.byKey(ValueKey('passwordTxtKey'));
+    await tester.enterText(passwordInput, 'password');
+
+    //tapping login button
+    var loginBtn = find.byKey(ValueKey('LoginSubmitBtnKey'));
+    await tester.tap(loginBtn);
+    await tester.pumpAndSettle();
+
+    //validating error message and current state
+    expect(find.text('Email id is required.'), findsNothing);
+    expect(find.text('Password is required.'), findsNothing);
+    expect(find.byType(HomeScreen), findsOneWidget);
+
+    //adding products to the cart
+    //await tester.tap(find.byType(ElevatedButton).first);
+    // await tester.tap(find.byKey(ValueKey('Armani button')));
+    await tester.tap(find.byType(ElevatedButton).at(2));
+    await tester.pumpAndSettle(Duration(seconds: 1));
+
+    //tapping on cart icon
+    await tester.scrollUntilVisible(find.byIcon(Icons.shopping_cart), 0.5);
+
+    await tester.tap(find.byIcon(Icons.shopping_cart));
+    await tester.pumpAndSettle(Duration(seconds: 5));
+    //await tester.tap(find.byKey(ValueKey('cartIcon')));
+    // await tester.pumpAndSettle(Duration(seconds: 5));
+    //await tester.tapAt(Offset(11, 11));
+    //await tester.pumpAndSettle(Duration(seconds: 5));
+
+    //validating user has moved to Cart Page
+    expect(find.byType(ProductCartPage), findsOneWidget);
+    expect(find.text('Remove from Cart'), findsOneWidget);
+
+    //removing the product from the cart
+    await tester.tap(find.byType(ElevatedButton).first);
+    await tester.pumpAndSettle(Duration(seconds: 1));
+
+    //validating item has been removed from the cart
+    expect(find.text('Cart is Empty'), findsOneWidget);
+  });
+}
