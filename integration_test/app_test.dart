c2cd22da7f0b31bfb4f0370f76e82771d5dd95d2
Siddharth Rath <siddharth@administrators-MacBook-Pro.local>
2021-08-07 18:56:56 +0530

    Minor update

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,8 +1,12 @@
 import './login_test.dart' as loginTest;
 import './addwatches_test.dart' as addWatches;
 import './cart_test.dart' as addToCart;
+import 'package:integration_test/integration_test.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding binding =
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized() as IntegrationTestWidgetsFlutterBinding;
+  binding.ensureVisualUpdate();
   loginTest.main();
   addWatches.main();
   addToCart.main();
