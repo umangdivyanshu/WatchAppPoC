d4f7d65416a3f934d9a8927460e561fca356b767
umangdivyanshu <udivyanshu8@gmail.com>
2021-09-28 13:56:25 +0530

    changes for json report generation

--- test_driver/integration_driver.dart
+++ test_driver/integration_driver.dart
@@ -1,3 +1,18 @@
-import 'package:integration_test/integration_test_driver.dart';
+//import 'package:integration_test/integration_test_driver.dart';
+import 'package:integration_test/integration_test_driver.dart'
+    as integration_test_driver;
 
-Future<void> main() => integrationDriver();
+//Future<void> main() => integrationDriver();
+Future<void> main() {
+  // Flutter Driver logs all messages to stderr by default so if this is run on a build server
+  // the process will fail due to writing errors. So handle this yourself for now
+  //driverLog = logDriverMessages;
+  // The Gherkin report data send back to this runner by the app after
+  // the tests have run will be saved to this directory
+  integration_test_driver.testOutputsDirectory =
+      'integration_test/gherkin/reports';
+
+  return integration_test_driver.integrationDriver(
+    timeout: const Duration(minutes: 90),
+  );
+}
